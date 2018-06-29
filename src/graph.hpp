5cab75fe352271e68b82dabcaf3f53f2ccd11ec7
Antoine <antoine.sb@orange.fr>
2018-06-29 16:02:13 +0100

    lots of stuff

--- src/graph.hpp
+++ src/graph.hpp
@@ -21,17 +21,20 @@
 
 #include <any>
 #include <exception>
+#include <iostream>
+#include <list>
 #include <map>
 #include <optional>
 #include <set>
 #include <string>
+#include <typeinfo>
 #include <utility>
 #include <vector>
-#include <typeinfo>
+
+#define DEBUG(message) std::cout << message << std::endl;
 
 namespace graph {
 	// data types are created at compile-time, so I have to hard code them
-
 	// vertex_signifiance: significative
 		// vertex_uniqueness: unique
 			// edges_uniqueness: unique
@@ -426,14 +429,202 @@ namespace graph {
 							}
 						// add vertices and edges
 							abstract_graph operator+(const abstract_graph& rhs) {
-								if(!isCompatible(rhs))
-									throw graph_incompatibility(properties, rhs.properties);
+								if(!isCompatible(rhs)) {
+									try {
+										throw graph_incompatibility(properties, rhs.properties);
+									}
+									catch(std::exception& e) {
+										std::cout << e.what() << std::endl;
+									}
+									std::cout << "not comp" << std::endl;
+								}
+								else
+									std::cout << "compatible" << std::endl;
+								/*
+								switch(properties.at("vertex_signifiance")) {
+									case significative:
+										if(v_u == unique_vertices) {
+											if(e_u == unique_edges) {
+												switch(w) {
+													case weighted:
+														container = VS_s_VU_u_EU_u_W_w_graph<vertex_data, edge_data>();
+														break;
+													case unweighted:
+														container = VS_s_VU_u_EU_u_W_uw_graph<vertex_data>();
+														break;
+													case mixed_weightness:
+														container = VS_s_VU_u_EU_u_W_mw_graph<vertex_data, edge_data>();
+														break;
+												}
+											}
+											else {
+												switch(w) {
+													case weighted:
+														container = VS_s_VU_u_EU_nu_W_w_graph<vertex_data, edge_data>();
+														break;
+													case unweighted:
+														container = VS_s_VU_u_EU_nu_W_uw_graph<vertex_data>();
+														break;
+													case mixed_weightness:
+														container = VS_s_VU_u_EU_nu_W_mw_graph<vertex_data, edge_data>();
+														break;
+												}
+											}
+										}
+										else {
+											if(e_u == unique_edges) {
+												switch(w) {
+													case weighted:
+														container = VS_s_VU_nu_EU_u_W_w_graph<vertex_data, edge_data>();
+														break;
+													case unweighted:
+														container = VS_s_VU_nu_EU_u_W_uw_graph<vertex_data>();
+														break;
+													case mixed_weightness:
+														container = VS_s_VU_nu_EU_u_W_mw_graph<vertex_data, edge_data>();
+														break;
+												}
+											}
+											else {
+												switch(w) {
+													case weighted:
+														container = VS_s_VU_nu_EU_nu_W_w_graph<vertex_data, edge_data>();
+														break;
+													case unweighted:
+														container = VS_s_VU_nu_EU_nu_W_uw_graph<vertex_data>();
+														break;
+													case mixed_weightness:
+														container = VS_s_VU_nu_EU_nu_W_mw_graph<vertex_data, edge_data>();
+														break;
+												}
+											}
+										}
+										break;
+									case not_significative:
+										if(v_u == unique_vertices) {
+											if(e_u == unique_edges) {
+												switch(w) {
+													case weighted:
+														container = VS_ns_VU_u_EU_u_W_w_graph<edge_data>();
+														break;
+													case unweighted:
+														container = VS_ns_VU_u_EU_u_W_uw_graph();
+														break;
+													case mixed_weightness:
+														container = VS_ns_VU_u_EU_u_W_mw_graph<edge_data>();
+														break;
+												}
+											}
+											else {
+												switch(w) {
+													case weighted:
+														container = VS_ns_VU_u_EU_nu_W_w_graph<edge_data>();
+														break;
+													case unweighted:
+														container = VS_ns_VU_u_EU_nu_W_uw_graph();
+														break;
+													case mixed_weightness:
+														container = VS_ns_VU_u_EU_nu_W_mw_graph<edge_data>();
+														break;
+												}
+											}
+										}
+										else {
+											if(e_u == unique_edges) {
+												switch(w) {
+													case weighted:
+														container = VS_ns_VU_nu_EU_u_W_w_graph<edge_data>();
+														break;
+													case unweighted:
+														container = VS_ns_VU_nu_EU_u_W_uw_graph();
+														break;
+													case mixed_weightness:
+														container = VS_ns_VU_nu_EU_u_W_mw_graph<edge_data>();
+														break;
+												}
+											}
+											else {
+												switch(w) {
+													case weighted:
+														container = VS_ns_VU_nu_EU_nu_W_w_graph<edge_data>();
+														break;
+													case unweighted:
+														container = VS_ns_VU_nu_EU_nu_W_uw_graph();
+														break;
+													case mixed_weightness:
+														container = VS_ns_VU_nu_EU_nu_W_mw_graph<edge_data>();
+														break;
+												}
+											}
+										}
+										break;
+									case mixed_signifiance:
+										if(v_u == unique_vertices) {
+											if(e_u == unique_edges) {
+												switch(w) {
+													case weighted:
+														container = VS_ms_VU_u_EU_u_W_w_graph<vertex_data, edge_data>();
+														break;
+													case unweighted:
+														container = VS_ms_VU_u_EU_u_W_uw_graph<vertex_data>();
+														break;
+													case mixed_weightness:
+														container = VS_ms_VU_u_EU_u_W_mw_graph<vertex_data, edge_data>();
+														break;
+												}
+											}
+											else {
+												switch(w) {
+													case weighted:
+														container = VS_ms_VU_u_EU_nu_W_w_graph<vertex_data, edge_data>();
+														break;
+													case unweighted:
+														container = VS_ms_VU_u_EU_nu_W_uw_graph<vertex_data>();
+														break;
+													case mixed_weightness:
+														container = VS_ms_VU_u_EU_nu_W_mw_graph<vertex_data, edge_data>();
+														break;
+												}
+											}
+										}
+										else {
+											if(e_u == unique_edges) {
+												switch(w) {
+													case weighted:
+														container = VS_ms_VU_nu_EU_u_W_w_graph<vertex_data, edge_data>();
+														break;
+													case unweighted:
+														container = VS_ms_VU_nu_EU_u_W_uw_graph<vertex_data>();
+														break;
+													case mixed_weightness:
+														container = VS_ms_VU_nu_EU_u_W_mw_graph<vertex_data, edge_data>();
+														break;
+												}
+											}
+											else {
+												switch(w) {
+													case weighted:
+														container = VS_ms_VU_nu_EU_nu_W_w_graph<vertex_data, edge_data>();
+														break;
+													case unweighted:
+														container = VS_ms_VU_nu_EU_nu_W_uw_graph<vertex_data>();
+														break;
+													case mixed_weightness:
+														container = VS_ms_VU_nu_EU_nu_W_mw_graph<vertex_data, edge_data>();
+														break;
+												}
+											}
+										}
+										break;
+								}
+								*/
+								/*
 								if(std::equal(properties.cbegin(), properties.cend(), rhs.properties.cbegin()))
 									container.merge(rhs.container);
+								*/
 								// check stuff
 
-
-								return this;
+								return *this;
 							}
 						// remove vertices and edges if they exist
 							abstract_graph* operator-(const abstract_graph& rhs) {
@@ -444,7 +635,7 @@ namespace graph {
 								// check the edges
 
 
-								return *this;
+								return this;
 							}
 					// bitwise operators
 						// invert links orientation in oriented graphs
@@ -452,7 +643,7 @@ namespace graph {
 								if(properties.at("orientation") == oriented) {
 
 								}
-								return *this;
+								return this;
 							}
 						// intersection
 							void operator&(const abstract_graph& rhs) {}
@@ -557,11 +748,26 @@ namespace graph {
 							inline bool operator||(const abstract_graph& rhs) { return !(container.empty() && rhs.container.empty()); }
 				// graph operations
 					// check compatibility of the properties
-						bool isCompatible(const abstract_graph& rhs) const { return properties = rhs.properties; }
+						bool isCompatible(const abstract_graph& rhs) const { return properties == rhs.properties; }
 					// tests whether there is an edge from the vertex x to the vertex y
 						bool adjacent(vertex_data vertex_1, vertex_data vertex_2, unsigned int index_1 = 0, unsigned int index_2 = 0) {
 							if(properties.at("orientation") == oriented) {
+								/*
+								container.at(vertex_1).second
+									std::max(vertex_1, vertex_2);
+								*/
+							}
+							else {
 
+							}
+							return false;
+						}
+						bool adjacent(unsigned int vertex_1, unsigned int vertex_2) {
+							if(properties.at("orientation") == oriented) {
+								/*
+								container.at(vertex_1).second
+									std::max(vertex_1, vertex_2);
+								*/
 							}
 							else {
 
@@ -569,13 +775,39 @@ namespace graph {
 							return false;
 						}
 					// adds the vertex x, if it is not there
-						template<class vertex_data = unsigned int>
-						auto add_vertex(const vertex_data& element) { return container.insert(element); }
+						std::optional<std::any> add_vertex(const vertex_data& element) {
+							if(properties.at(orientation) == oriented) {
+
+							}
+							/*
+							if(auto result = container.insert(element); result.second)
+								return result.first;
+							*/
+							return {};
+						}
+						std::optional<std::any> add_vertex(vertex_data&& element) {
+							/*
+							if(auto result = container.insert(element); result.second)
+								return result.first;
+							*/
+							return {};
+						}
+					// lists all vertices y such that there is an edge from the vertex x to the vertex y
+						std::list<std::any> neightbors(const vertex_data& element) const {
+
+						}
+						std::list<std::any> neightbors(vertex_data&& element) const {
+
+						}
+						std::list<std::any> neightbors(std::set<std::any>::iterator& element) const {
+
+						}
+						std::list<std::any> neightbors(std::map<std::any, std::any>::iterator& element) const {
+
+						}
 			/*
 			The basic operations provided by a graph data structure G usually include:
 				adjacent(x, y): tests whether there is an edge from the vertex x to the vertex y
-				neighbors(x): lists all vertices y such that there is an edge from the vertex x to the vertex y
-				add_vertex(x): adds the vertex x, if it is not there
 				remove_vertex(x): removes the vertex x, if it is there
 				add_edge(x, y): adds the edge from the vertex x to the vertex y, if it is not there
 				remove_edge(x, y): removes the edge from the vertex x to the vertex y, if it is there
@@ -586,6 +818,149 @@ namespace graph {
 				get_edge_value(x, y): returns the value associated with the edge (x, y)
 				set_edge_value(x, y, v): sets the value associated with the edge (x, y) to v
 			*/
+
+			void* casted_container() {
+				switch(properties.at("vertex_signifiance")) {
+					case significative:
+						if(properties.at("vertex_uniqueness") == unique_vertices) {
+							if(properties.at("edge_uniqueness") == unique_edges) {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_s_VU_u_EU_u_W_w_graph<vertex_data, edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_s_VU_u_EU_u_W_uw_graph<vertex_data>>(container);
+									case mixed_weightness:
+										return &std::any_cast<VS_s_VU_u_EU_u_W_mw_graph<vertex_data, edge_data>>(container);
+								}
+							}
+							else {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_s_VU_u_EU_nu_W_w_graph<vertex_data, edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_s_VU_u_EU_nu_W_uw_graph<vertex_data>>(container);
+									case mixed_weightness:
+										return &std::any_cast<VS_s_VU_u_EU_nu_W_mw_graph<vertex_data, edge_data>>(container);
+								}
+							}
+						}
+						else {
+							if(properties.at("edge_uniqueness") == unique_edges) {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_s_VU_nu_EU_u_W_w_graph<vertex_data, edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_s_VU_nu_EU_u_W_uw_graph<vertex_data>>(container);
+									case mixed_weightness:
+										return &std::any_cast< VS_s_VU_nu_EU_u_W_mw_graph<vertex_data, edge_data>>(container);
+								}
+							}
+							else {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_s_VU_nu_EU_nu_W_w_graph<vertex_data, edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_s_VU_nu_EU_nu_W_uw_graph<vertex_data>>(container);
+									case mixed_weightness:
+										return &std::any_cast<VS_s_VU_nu_EU_nu_W_mw_graph<vertex_data, edge_data>>(container);
+								}
+							}
+						}
+						break;
+					case not_significative:
+						if(properties.at("vertex_uniqueness") == unique_vertices) {
+							if(properties.at("edge_uniqueness") == unique_edges) {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_ns_VU_u_EU_u_W_w_graph<edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_ns_VU_u_EU_u_W_uw_graph>(container);
+									case mixed_weightness:
+										return &std::any_cast<VS_ns_VU_u_EU_u_W_mw_graph<edge_data>>(container);
+								}
+							}
+							else {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_ns_VU_u_EU_nu_W_w_graph<edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_ns_VU_u_EU_nu_W_uw_graph>(container);
+									case mixed_weightness:
+										return &std::any_cast<VS_ns_VU_u_EU_nu_W_mw_graph<edge_data>>(container);
+								}
+							}
+						}
+						else {
+							if(properties.at("edge_uniqueness") == unique_edges) {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_ns_VU_nu_EU_u_W_w_graph<edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_ns_VU_nu_EU_u_W_uw_graph>(container);
+									case mixed_weightness:
+										return &std::any_cast<VS_ns_VU_nu_EU_u_W_mw_graph<edge_data>>(container);
+								}
+							}
+							else {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_ns_VU_nu_EU_nu_W_w_graph<edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_ns_VU_nu_EU_nu_W_uw_graph>(container);
+									case mixed_weightness:
+										return &std::any_cast<VS_ns_VU_nu_EU_nu_W_mw_graph<edge_data>>(container);
+								}
+							}
+						}
+						break;
+					case mixed_signifiance:
+						if(properties.at("vertex_uniqueness") == unique_vertices) {
+							if(properties.at("edge_uniqueness") == unique_edges) {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_ms_VU_u_EU_u_W_w_graph<vertex_data, edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_ms_VU_u_EU_u_W_uw_graph<vertex_data>>(container);
+									case mixed_weightness:
+										return &std::any_cast<VS_ms_VU_u_EU_u_W_mw_graph<vertex_data, edge_data>>(container);
+								}
+							}
+							else {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_ms_VU_u_EU_nu_W_w_graph<vertex_data, edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_ms_VU_u_EU_nu_W_uw_graph<vertex_data>>(container);
+									case mixed_weightness:
+										return &std::any_cast<VS_ms_VU_u_EU_nu_W_mw_graph<vertex_data, edge_data>>(container);
+								}
+							}
+						}
+						else {
+							if(properties.at("edge_uniqueness") == unique_edges) {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_ms_VU_nu_EU_u_W_w_graph<vertex_data, edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_ms_VU_nu_EU_u_W_uw_graph<vertex_data>>(container);
+									case mixed_weightness:
+										return &std::any_cast<VS_ms_VU_nu_EU_u_W_mw_graph<vertex_data, edge_data>>(container);
+								}
+							}
+							else {
+								switch(properties.at("weightness")) {
+									case weighted:
+										return &std::any_cast<VS_ms_VU_nu_EU_nu_W_w_graph<vertex_data, edge_data>>(container);
+									case unweighted:
+										return &std::any_cast<VS_ms_VU_nu_EU_nu_W_uw_graph<vertex_data>>(container);
+									case mixed_weightness:
+										return &std::any_cast<VS_ms_VU_nu_EU_nu_W_mw_graph<vertex_data, edge_data>>(container);
+								}
+							}
+						}
+						break;
+				}
+			}
 	};
 }
 
