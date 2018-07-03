4a9aff118fbe22f74e5811e689529eff2ddbaac0
Antoine <antoine.sb@orange.fr>
2018-07-03 22:20:56 +0100

    Change of the structure

--- src/dijkstra.hpp
+++ src/dijkstra.hpp
@@ -1,6 +1,6 @@
 /*
  * @author Antoine "Anthony" Sébert
- * @description An implementation of the Dijkstra algorithm in C++17 as a class - header
+ * @description An implementation of the Dijkstra's algorithm in C++17 as a single header library
  * @note no need to specialize std::swap, because it uses the move constructor and move assignment operator
  * @note in this implementation, the weights are positives
  * @date 01/04/2018
@@ -13,10 +13,16 @@
  *	6  insert u at the beginning of S            // Push the source onto the stack
  */
 
-#pragma once
+#if 201703L <= __cplusplus
+	#error This library needs at least a C++17 compliant compiler
+#endif
+
+#ifndef DIJKSTRA_HPP
+#define DIJKSTRA_HPP
 
 #include <any>
 #include <algorithm>
+#include <cassert>
 #include <chrono>
 #include <iostream>
 #include <limits>
@@ -29,60 +35,41 @@
 
 #include "../../fiboheap/fiboheap.hpp"
 
-namespace dijkstra_algorithm {
-	template<typename edge_data = unsigned int>
-	using graph_test = std::set<std::map<std::set<std::any>::const_iterator, edge_data>>;
+namespace std {
+	template<class Iter>
+	void check_range(Iter first, Iter second) {
+		static_cast<void>(first == second); // ill-formed compilation if iterators are not comparable, making it fail
+		// throw range_error("invalid iterator range");
+		//return first < second;
+	}
+
+	any dijkstra(set<map<set<any>::const_iterator, int>>::iterator _Start, set<map<set<any>::const_iterator, int>>::iterator _Arr) {
+		check_range(_Start, _Arr);
+
+		FibHeap<int> queue = FibHeap<int>();
+
+		//cout << typeid(*_Start).name() << endl;
+		/*
+		dist[source] ← 0                           // Initialization
+		create vertex set Q
+		for each vertex v in Graph :
+			if v ≠ source
+				dist[v] ← INFINITY                 // Unknown distance from source to v
+			prev[v] ← UNDEFINED                    // Predecessor of v
+			Q.add_with_priority(v, dist[v])
+			while Q is not empty:                      // The main loop
+				u ← Q.extract_min()                    // Remove and return best vertex
+				for each neighbor v of u :              // only v that is still in Q
+					alt ← dist[u] + length(u, v)
+					if alt < dist[v]
+						dist[v] ← alt
+						prev[v] ← u
+						Q.decrease_priority(v, alt)
+		return dist, prev
+		*/
+
+		return any();
+	}
 }
 
-namespace dijkstra_algorithm {
-	class Dijkstra {
-		/* ATTRIBUTES */
-			private:
-				graph_test<std::any>* graphContainer;
-				//typename graph_test<edge_data>::const_pointer start, finish;
-				FibHeap<std::any> queue;
-		/* MEMBERS */
-			public:
-				// constructors
-					template<typename T>
-					Dijkstra(graph_test<T>* data) : queue(FibHeap<T>()) {
-
-					}
-					Dijkstra(const Dijkstra& other) : graphContainer(graphContainer) {}
-					Dijkstra(Dijkstra&& other) noexcept : graphContainer(graphContainer) { /* destroy eventual dynamic data */ }
-				// destructors
-					~Dijkstra() noexcept/* = default*/ {} // maybe not need to redefine
-				// operators
-					Dijkstra& operator=(const Dijkstra& other) { return (*this = std::move(Dijkstra(other))); }
-					Dijkstra& operator=(Dijkstra&& other) noexcept {
-						if(this == &other)
-							return *this;
-
-						graphContainer = other.graphContainer;
-						/* destroy eventual dynamic data */
-
-						return *this;
-					}
-			protected:
-				void run() {
-					/*
-					dist[source] ← 0                           // Initialization
-					create vertex set Q
-					for each vertex v in Graph :
-						if v ≠ source
-							dist[v] ← INFINITY                 // Unknown distance from source to v
-						prev[v] ← UNDEFINED                    // Predecessor of v
-						Q.add_with_priority(v, dist[v])
-						while Q is not empty:                      // The main loop
-							u ← Q.extract_min()                    // Remove and return best vertex
-							for each neighbor v of u :              // only v that is still in Q
-								alt ← dist[u] + length(u, v)
-								if alt < dist[v]
-									dist[v] ← alt
-									prev[v] ← u
-									Q.decrease_priority(v, alt)
-					return dist, prev
-					*/
-				}
-	};
-}
+#endif
