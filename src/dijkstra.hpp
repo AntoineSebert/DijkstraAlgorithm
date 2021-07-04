18137309235c4fec7e8167bd1e37e7597074ced2
Antoine <antoine.sb@orange.fr>
2021-07-04 17:55:08 +0200

    remake + finish + test

--- src/dijkstra.hpp
+++ src/dijkstra.hpp
@@ -1,107 +1,175 @@
-/*
- * @author Antoine "Anthony" Sébert
- * @description Implementation of Dijkstra's algorithm in C++17 as a single header library using a fibonacci heap and named tuples
- * @note in this implementation, the weights are positives
- * @date 01/04/2018
- * @todo Spécialisation de l'algorithme : arrêter la recherche lorsque l'égalité s(1) = s(fin) est vérifiée, dans le cas où on ne cherche que la distance minimale entre s(deb) et s(fin)
- *	1  S ← empty sequence
- *	2  u ← target
- *	3  while prev[u] is defined:                 // Construct the shortest path with a stack S
- *	4      insert u at the beginning of S        // Push the vertex onto the stack
- *	5      u ← prev[u]                           // Traverse from target to source
- *	6  insert u at the beginning of S            // Push the source onto the stack
- *	https://www.cl.cam.ac.uk/teaching/1112/AlgorithII/1987-FredmanTar-fibonacci.pdf
- */
-
-#if 201703L <= __cplusplus
-	#error This library needs at least a C++17 compliant compiler
-#endif
-
 #ifndef DIJKSTRA_HPP
 #define DIJKSTRA_HPP
 
-#include <any>
+#include <algorithm>
 #include <cassert>
-#include <chrono>
-#include <iostream>
+#include <filesystem>
+#include <functional>
 #include <limits>
 #include <map>
+#include <memory>
 #include <optional>
 #include <set>
-#include <unordered_set>
+#include <string>
+#include <type_traits>
 #include <utility>
+#include <vector>
 
-#include "../../fiboheap/fiboheap.hpp"
+#include "boost/heap/fibonacci_heap.hpp"
 
-namespace std {
-	template<typename T>
-	using graph = set<map<unsigned int, T>>;
+/*
+ * @author Antoine Sébert
+ * @description Implementation of Dijkstra's algorithm in C++17 as a single
+ * header library using a fibonacci heap
+ */
+namespace dijkstra {
+	using namespace std;
+	using namespace boost::heap;
+	namespace fs = std::filesystem;
 
-	template<typename T>
-	using graph_iterator = typename graph<T>::iterator;
+	enum class Direction : uint8_t {
+		DIRECTED,
+		UNDIRECTED,
+	};
 
-	template<typename T>
-	using graph_const_iterator = typename graph<T>::const_iterator;
-}
-
-namespace std {
-	template <class T, class = void>
-	struct is_iterator : false_type {};
-	template <class T>
-	struct is_iterator<T, void_t<typename iterator_traits<T>::iterator_category>> : true_type { };
-
-	template<typename Iter>
-	bool check_range(const Iter first, const Iter second) {
-		static_cast<void>(first == second); // ill-formed compilation if iterators are not comparable, making it fail
-		return is_iterator<Iter>();
-	}
-
-	template<typename T = unsigned int>
-	pair<optional<any>, chrono::duration<double>> dijkstra(const graph<typename T>& data, graph_iterator<typename T> Source, graph_iterator<typename T> Arr) {
-		/*
-		if(data.empty() == false)
-			return pair<optional<any>, chrono::duration<double>>();
-		*/
-		// Initialization
-		check_range(Source, Arr);
-
-		auto start = chrono::high_resolution_clock::now();
-
-		fibonacci_heap::fibonacci_heap<unsigned int> queue = fibonacci_heap::fibonacci_heap<unsigned int>();
-		vector<T> distances(data.size(), numeric_limits<unsigned int>::max());
-		//distances.at(distance(Source, data.begin())) = 0;
-		unordered_set<optional<unsigned int>> path;
-
-		assert(data.size() == distances.size());
-
-		unsigned int i = 0;
-		for(const auto& element : data) {
-			//typeid(element).name();
-			//queue.push(element, distances.at(i));
-			++i;
-		}
-
-
-		while(!queue.empty()) {												// The main loop
-			auto u = queue.extract_min();									// Remove and return best vertex
-			//for(const auto& vertex : (data.begin() + u->payload)) {								// only v that is still in Q
-				/*
-				if(unsigned int alt = dist[u] + length(u, vertex); alt < dist[vertex]) {
-					dist[vertex] = alt;
-					prev[vertex] = u;
-					queue.decrease_priority(vertex, alt);
+	template <typename W, class = enable_if_t<is_integral_v<W>>>
+	class Graph {
+		public:
+			struct Node {
+				map<string, W> neighbors = {};
+			};
+			struct comparator {
+				bool operator() (const pair<string, W>& lhs, const pair<string, W>& rhs) const {
+					return get<1>(lhs) > get<1>(rhs);
 				}
-				*/
-			//}
-		}
+			};
+		private:
+			map<string, Node> nodes;
+			Direction dir;
 
-		auto end = chrono::high_resolution_clock::now();
-		chrono::duration<double> elapsed_time = end - start;
-		cout << "elapsed time: " << elapsed_time.count() << "s" << endl;
+		public:
+			Graph(Direction _dir = Direction::UNDIRECTED) noexcept : nodes({}), dir(_dir) {}
 
-		//return dist, prev
-		return pair<optional<any>, chrono::duration<double>>();
-	}
-}
+			Direction get_direction() const noexcept {
+				return dir;
+			}
+
+			const map<string, Node>& get_nodes() const noexcept {
+				return nodes;
+			}
+
+			void add_node(string label) {
+				if (!contains(label))
+					nodes[label] = {};
+			}
+
+			Graph add_node(string label, const map<string, W>& neighbors) {
+				add_node(label);
+
+				for (const auto& [neighbor, weight] : neighbors)
+					add_edge(label, neighbor, weight);
+
+				if (dir == Direction::UNDIRECTED)
+					for (const auto& [neighbor, weight] : neighbors)
+						add_edge(neighbor, label, weight);
+			}
+
+			void remove_node(string label) {
+				nodes.erase(label);
+
+				for (auto& [_, node] : nodes)
+					node.neighbors.erase(label);
+			}
+
+			void add_edge(string label0, string label1, W weight = W(0)) {
+				add_node(label0);
+				add_node(label1);
+
+				nodes.at(label0).neighbors[label1] = weight;
+
+				if (dir == Direction::UNDIRECTED)
+					nodes.at(label1).neighbors[label0] = weight;
+			}
+
+			bool remove_edge(string label0, string label1) {
+				if (has_neighbor(label0, label1)) {
+					nodes.at(label0).neighbors.erase(label1);
+
+					if (dir == Direction::UNDIRECTED && has_neighbor(label1, label0))
+						nodes.at(label1).neighbors.erase(label0);
+
+					return true;
+				}
+				
+				return false;
+			}
+
+			inline bool contains(string label) const {
+				return nodes.find(label) != nodes.end();
+			}
+
+			bool empty() const {
+				return nodes.empty();
+			}
+
+			bool has_neighbor(string label0, string label1) const {
+				return contains(label0) && contains(label1)
+					&& nodes.at(label0).neighbors.find(label1) != nodes.at(label0).neighbors.end();
+			}
+
+			fs::path find(string src, string dst) const {
+				assertions(src, dst);
+
+				auto preds = map<string, string>();
+				auto heap = fibonacci_heap<pair<string, W>, compare<comparator>>();
+				auto handles = map<string, decltype(heap)::handle_type>();
+
+				for (auto& [label, _] : nodes)
+					handles[label] = heap.push(make_pair(label, label == src ? 0 : numeric_limits<W>::max()));
+
+				while (!heap.empty()) {
+					auto& [nearest, distance] = heap.top();
+
+					if (nearest == dst)
+						break;
+
+					for (auto& [neighbor, weight] : nodes.at(nearest).neighbors)
+						if (handles.find(neighbor) != handles.end())
+							if (W alt = distance + weight; alt < get_distance(heap, neighbor)) {
+								preds[neighbor] = nearest;
+								heap.update(handles.at(neighbor), make_pair(neighbor, alt));
+							}
+
+					handles.erase(nearest);
+					heap.pop();
+				}
+
+				return preds_to_path(preds, src, dst);
+			}
+
+		private:
+			W get_distance(fibonacci_heap<pair<string, W>, compare<comparator>>& heap, string label) const {
+				return find_if(heap.begin(), heap.end(), [&](const auto& e) { return get<0>(e) == label; })->second;
+			}
+			void assertions(string src, string dst) const {
+				assert(("The graph is empty", !empty()));
+				assert(("The source node is not in the graph", contains(src)));
+				assert(("The destination node is not in the graph", contains(dst)));
+				assert(("The source and destinatino are the same", dst != src));
+			}
+
+			fs::path preds_to_path(const map<string, string>& preds, string src, string dst) const {
+				vector<string> reverse_path = { dst };
+				while (reverse_path.back() != src)
+					reverse_path.push_back(preds.at(reverse_path.back()));
+
+				auto _path = fs::path();
+				for (auto it = reverse_path.rbegin(); it != reverse_path.rend(); it++)
+					_path /= *it;
+
+				return _path;
+			}
+	};
+};
 
 #endif
